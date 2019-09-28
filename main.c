a9a0e1bdf06f62dbdd27a4e35b5d47e8248c6273
asad8389 <48471395+asad8389@users.noreply.github.com>
2019-09-28 14:00:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 #include<conio.h>
 int main()
 {
-int c=0,p,n;
+int cmp=0,p,n;
 
-printf("Total sticks=21\nyou can pick 1,2,3,4 sticks at a time\n");
+printf("Total sticks=21\nyou can pick 4 sticks at a time\n");
 
 
 for(n=21;n>1;)
 {
-    printf("\nits your turn\n");
+    printf("\nits your turn, pick sticks\n");
     scanf("%d",&p);
 
       if(p<1 || p>4)
       {
-            printf("\nwrong pick try again\n");
+            printf("\nwrong pick, try again\n");
       }
       else
       {
-            c=5-p;
+            cmp=5-p;
             printf("i pick %d sticks",c);
-            n=n-(c+p);
+            n=n-(cmp+p);
             printf("\nremaining sticks=%d\n",n);
             if(n==1)
             printf("\nyou loose");
