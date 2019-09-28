0a30b4cf3887c748e9f31af820dca610ea530ac2
asad8389 <48471395+asad8389@users.noreply.github.com>
2019-09-28 14:16:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ for(n=21;n>1;)
     printf("\nits your turn, pick sticks\n");
     scanf("%d",&p);
 
-      if(p<1 || p>4)
+      if(p<1 && p>4)
       {
             printf("\nwrong pick, try again\n");
       }
       else
       {
             cmp=5-p;
-            printf("i pick %d sticks",c);
+            printf("i pick %d sticks",cmp);
             n=n-(cmp+p);
             printf("\nremaining sticks=%d\n",n);
             if(n==1)
